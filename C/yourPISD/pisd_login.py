8170ed0a2e558db901701a58a663ea1c2b403261
enricozb <Enrico@Enricos-MBP.home>
2015-02-08 11:17:03 -0600

    renamed to onePISD

--- C/yourPISD/pisd_login.py
+++
@@ -1,9 +0,0 @@
-import pisd
-import mechanize
-import os
-
-print("Running From %s" % os.getcwd())
-
-session = pisd.PISD_Session('enrico.borba.1','inferno&7', True)
-session.login()
-session.goto_gradesummary()
