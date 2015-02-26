c28612f5a60295cb5180a4b2d4dfa26e820c3a85
Enrico Borba <enricozb@gmail.com>
2015-02-25 20:49:25 -0600

    reg update

--- gpush.sh
+++ gpush.sh
@@ -2,9 +2,9 @@
 
 project="$1"
 
-while [ "$project" != "master" ] && [ "$project" != "onePISD" ]
+while [ "$project" != "master" ] && [ "$project" != "onePISD" ] && [ "$project" != "transient" ]
 do
-	echo -n "	Select project (master, onePISD): "
+	echo -n "	Select project (master, onePISD, transient): "
 	read project
 done
 
@@ -22,15 +22,17 @@ git config --global user.email enricozb@gmail.com
 if [ "$project" == "master" ]
 then 
 	cd ~/Documents/SublimeFiles/master
-	git rm -r --cache .
-	git add .
-	git commit -m "$msg"
-	git push -u origin master
+
 elif [ "$project" == "onePISD" ]
 then
 	cd ~/Documents/SublimeFiles/onePISD
-	git rm -r --cache .
-	git add .
-	git commit -m "$msg"
-	git push -u origin master
+	
+elif [ "$project" != "transient" ]
+then 
+	cd ~/Documents/SublimeFiles/transient
 fi
+
+git rm -r --cache .
+git add .
+git commit -m "$msg"
+git push -u origin master
